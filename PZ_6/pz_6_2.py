32c4185aa5464a1e3a50ebff1fa92dbeef429ba7
Zhelyak_Maria <112765207+Maria099@users.noreply.github.com>
2022-11-30 12:03:19 +0300

    Delete PZ_6 directory

--- PZ_6/pz_6_2.py
+++
@@ -1,8 +0,0 @@
-from random import sample
-n, k, l = [int(input('Введите значение "N, K, L" через Enter:\n')) for i in range(3)]
-# n, k, l = [int(i) for i in input("Введите значение через запятую: \n").split(',')]
-lst = sample(range(1, 100), n)
-k += 1
-print(*lst, sep=' | ')
-lst[k:l] = lst[k:l][::-1]
-print(*lst, sep=' | ')
